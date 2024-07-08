45b0114f184e6f34cb1ccc1fa3128d8e23e7a37d
Nicolás Andrés González González <133883532+nicolasAG2@users.noreply.github.com>
2024-07-08 02:04:07 -0400

    Add files via upload

--- neekopedia/settings.py
+++ neekopedia/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clients',
     'bootstrap5',
+    'administrador',
 ]
 
 MIDDLEWARE = [
--- neekopedia/urls.py
+++ neekopedia/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clients.urls')),
     path('accounts/', include("django.contrib.auth.urls")),
+    path('administrador/', include('administrador.urls')),
 ]
